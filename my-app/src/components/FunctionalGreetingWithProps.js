d4856d4681ff00cbab0030a4cab8ccbdcab637a4
magzdev <magdakloskowicz@gmail.com>
2024-12-13 00:21:23 +0000

    add a name prop

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FunctionalGreetingWithProps = (props) => {
     console.log(props);
-    return <h1>Hello, {props.greeting}</h1>
+    return <h1>Hello {props.name}, {props.greeting}</h1>
 };
 
 export default FunctionalGreetingWithProps;
